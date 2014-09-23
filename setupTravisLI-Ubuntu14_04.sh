db4217269afd4a51baaee574da43c5bda0e3da23
Praween AMONTAMAVUT <not001praween001@gmail.com>
2014-09-23 23:58:38 +0900

    Add travis-li setup shell.

--- setupTravisLI-Ubuntu14_04.sh
+++ setupTravisLI-Ubuntu14_04.sh
@@ -1,2 +1,3 @@
 sudo sh -c "gem install travis-lint"
-sudo sh -c "ln -s /usr/local/bin/travis-lint /usr/local/bin/travis"
+#sudo sh -c "ln -s /usr/local/bin/travis-lint /usr/local/bin/travis"
+sudo sh -c "gem install travis"
